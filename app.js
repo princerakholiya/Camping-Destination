deac7aecbe4ab86928fa32cf2abd2d7476debd40
princerakholiya <202051147@iiitvadodara.ac.in>
2022-02-23 22:01:36 +0530

    Added Basic Styles

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app =express()
 //const path = require('path')
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground.js')
 const methodOverride = require('method-override')
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -16,6 +17,7 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine','ejs')
 //app.set('views' , path.join(__dirname , 'viwes'))
 app.use(express.urlencoded({extended :true}))
