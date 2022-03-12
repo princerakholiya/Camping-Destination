0479c02bd5e07ea9b5d312741544678036c21524
princerakholiya <202051147@iiitvadodara.ac.in>
2022-03-12 19:46:55 +0530

    Added controllers and Star Ratings

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,25 +7,10 @@ const Review = require('../models/review.js')
 const ExpressError = require('../utils/ExpressError');
 const { validateReview , isLoggedIn , isReviewAuthor} = require('../middleware.js')
 
-
+const reviews = require('../controllers/reviews');
  
 
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-     review.author=req.user._id
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created new review!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review')
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports=router
